d74f396a3f08fb155ee77c9ada657eb700c32a05
LidiaBa <95959174+LidiaBa@users.noreply.github.com>
2022-12-26 13:08:39 +0300

    Add files via upload

---
+++ oop3/src/com/company/Exception.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class Exception extends java.lang.Exception {
+    public Exception(String message){super(message);}
+}
---
+++ oop3/src/com/company/Prodavez.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Prodavez implements Printer{
+    private String name;
+    private int indif;
+
+    public Prodavez (int indif, String name){
+        this.indif = indif;
+        this.name = name;
+    }
+    @Override
+    public void print() {
+        System.out.println("Продавец " + indif +":"+ " Имя: " + name);
+    }
+}
+
+
+
+
+
+
+
+
